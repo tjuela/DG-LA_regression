d0591fcc8293c5f836fa48a43f94de9a1d1f031d
oaspelin <oskar.aspelin@aalto.fi>
2017-01-15 19:34:15 +0200

    should be right format for kaggle submission

--- classifierCompleteFlow.R
+++ classifierCompleteFlow.R
@@ -90,7 +90,7 @@ library(caret)
   SS.residual   <- sum((test.y - test.pred)^2)
   SS.regression <- sum((test.pred - mean(test.y))^2)
   SS.total <- (SS.regression+SS.residual)
-  
+    
   # NOT the fraction of variability explained by the model
   test.rsq <- 1 - SS.residual/SS.total  
   test.rsq
@@ -102,8 +102,8 @@ library(caret)
 #         step 2.1: Use classifier to predict progress for test data
 #======================================================================== 
   
-  testDb=read.csv('OutputTable_test.csv', stringsAsFactors = F)
-  testDb$Grade=NULL; testDb$GradeDiff=NULL;
+  testDb=read.csv('features_test.csv', stringsAsFactors = F)
+  testDb$OveralGradeDiff=NULL
   testDb[is.na(testDb)]=0
   
   #---- use trained model to predict progress for test data
@@ -113,18 +113,17 @@ library(caret)
 #         step 2.1: prepare submission file for kaggle
 #======================================================================== 
   
-  cl.Results=testDb[,c('ProblemID', 'UserID', 'SubmissionNumber')]
+  cl.Results=testDb[,c('ProblemID', 'UserID')]
   cl.Results$improved=preds
-  levels(cl.Results$improved)=c(0,1) # 
-  cl.Results$uniqRowID= paste0(cl.Results$UserID,'_', cl.Results$ProblemID,'_', cl.Results$SubmissionNumber)
+  cl.Results$uniqRowID= paste0(cl.Results$UserID,'_', cl.Results$ProblemID)
   cl.Results=cl.Results[,c('uniqRowID','improved')]
   table(cl.Results$improved)
   
   #----- keep only rows which are listed in classifier_templtae.csv file
   #----- this excludes first submissions and cases with no forum and video event in between two submissions
-  classifier_templtaete= read.csv('classifier_templtae.csv', stringsAsFactors = F)
-  kaggleSubmission=merge(classifier_templtaete,cl.Results )
-  write.csv(kaggleSubmission,file='classifier_results.csv', row.names = F)
+  regression_template= read.csv('regression_template.csv', stringsAsFactors = F)
+  kaggleSubmission=merge(regression_template,cl.Results )
+  write.csv(kaggleSubmission,file='regression_results.csv', row.names = F)
   
   
   #------- submit the resulting file (classifier_results.csv) to kaggle 
--- datasets/aggregateFeatures.R
+++ datasets/aggregateFeatures.R
@@ -31,3 +31,36 @@ View(db)
 #------ save feature file
   write.csv(agg.features, file='features.csv')
  
+  
+  
+#SAME FOR TEST  
+  #------ read data frame
+  db=read.csv('datasets/OutputTable_test.csv')
+  #------ sort submissions
+  db=db[order(db$UserID,db$ProblemID,db$SubmissionNumber),]
+  dim(db)
+  View(db)
+  #------- aggregate by UserID and ProblemID ---------
+  length(unique(db$UserID))
+  agg.features=ddply(db, .(UserID,ProblemID), summarise, 
+                     overalGradeDiff=Grade[length(Grade)]-Grade[1], 
+                     countOfSubmissions=length(SubmissionNumber),
+                     totalTime = sum(TimeSinceLast, na.rm = T),
+                     totalVideoTime= sum(DurationOfVideoActivity,na.rm=T),
+                     countOfVideoEvents = sum(NVideoEvents,na.rm = T),
+                     countOfForumEvents = sum(NForumEvents,na.rm = T))
+  # countOfVideoAndForumEvents= (sum(NVideoEvents,na.rm = T)+sum(NForumEvents,na.rm = T)))
+  
+  
+  #-------normalize on submissions---------
+  agg.features["VideoPerSubmission"]<-NA
+  agg.features$VideoPerSubmission<-(agg.features$countOfVideoEvents/agg.features$countOfSubmissions)
+  agg.features["ForumPerSubmission"]<-NA
+  agg.features$ForumPerSubmission<-(agg.features$countOfForumEvents/agg.features$countOfSubmissions)
+  agg.features["VideoTimePerSubmission"]<-NA
+  agg.features$VideoTimePerSubmission<-(agg.features$totalVideoTime/agg.features$countOfSubmissions)
+  
+  #------ remove cases with only one attempt
+  agg.features=filter(agg.features,countOfSubmissions>1); dim(agg.features)
+  #------ save feature file
+  write.csv(agg.features, file='features_test.csv')
